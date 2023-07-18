a07ff05976767c6b0b098cd75af0800cf82a6702
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-18 12:01:03 -0400

    I did client-side code. Need to adjust to work on a server. My bad.

--- controllers/weather_api_routes.js
+++ controllers/weather_api_routes.js
@@ -9,11 +9,13 @@ const date = "2023-07-18";
 const apiKey = WEATHER_API_KEY;
 const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(location)}&dt=${new Date(date).getTime() / 1000}&units=imperial&appid=${apiKey}`;
 const weatherInfoEl = document.getElementById("weather-info");
+const weatherIconEl = document.getElementById("weather-icon");
 
 function fetchWeatherData(location) {
     fetch(weatherUrl)
         .then((response) => response.json())
         .then((weatherData) => {
+            weatherIconEl.innerHTML = `<img src="https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png" alt="Weather Icon">`;
             weatherInfoEl.innerHTML = `
             <p>Location: ${weatherData.name}</p>
             <p>Date: ${weatherData.date}</p>
