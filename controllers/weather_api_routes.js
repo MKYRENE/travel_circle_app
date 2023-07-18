77957f73c994c703ea7841ef0c252c8e76e268ce
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-18 11:29:24 -0400

    Working on implementing OpenWeatherMap API to display weather

---
+++ controllers/weather_api_routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+
+// User provides this info
+const location = "London";
+const date = "2023-07-18";
+
+// This is for the function
+const apiKey = WEATHER_API_KEY;
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(location)}&dt=${new Date(date).getTime() / 1000}&units=imperial&appid=${apiKey}`;
+const weatherInfoEl = document.getElementById("weather-info");
+
+function fetchWeatherData(location) {
+    fetch(weatherUrl)
+        .then((response) => response.json())
+        .then((weatherData) => {
+            weatherInfoEl.innerHTML = `
+            <p>Location: ${weatherData.name}</p>
+            <p>Date: ${weatherData.date}</p>
+            <p>Temperature: ${weatherData.main.temp}</p>
+            <p>Humidity: ${weatherData.main.humidity}</p>
+            <p>Wind Speed: ${weatherData.wind.speed}</p>
+            `;
+        })
+        .catch((error) => console.error("Error:", error));
+}
+
+module.exports = router;
