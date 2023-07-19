37783f3122545919557e69a7ebc65dd1c64c3277
MKYRENE <rodriguezm90@icloud.com>
2023-07-19 12:06:55 -0400

    updated css file and footer on main.hbs file for contacts on index, about, and contact.

--- controllers/map_api.js
+++ controllers/map_api.js
@@ -1,39 +1,45 @@
+// Import the Express router module to create routes
 const router = require("express").Router();
 
+// Immediately-invoked function expression (IIFE) to encapsulate the code and avoid polluting the global scope
 (function () {
-    var mapBoxKey = MAP_API_KEY
+    // Retrieve the Mapbox API key stored in the environment variable MAP_API_KEY
+    var mapBoxKey = MAP_API_KEY;
 
-
-    // Initialize the map
+    // Initialize the map using the Mapbox API key
     mapboxgl.accessToken = mapBoxKey;
     var map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: [-95.7129, 37.0902], // Set the initial center of the map
+        container: "map", // The container ID where the map will be rendered
+        style: "mapbox://styles/mapbox/streets-v11", // The map style URL
+        center: [-95.7129, 37.0902], // Set the initial center of the map to longitude -95.7129 and latitude 37.0902
         zoom: 3, // Set the initial zoom level of the map
     });
 
+    // Add a navigation control to the top-right corner of the map
     const nav = new mapboxgl.NavigationControl();
     map.addControl(nav, "top-right");
 
-
-    // Function to handle search
+    // Function to handle geocoding (converting search query to coordinates)
     function geocode(query) {
         var geocoder = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(query) + '.json?access_token=' + mapboxgl.accessToken;
 
+        // Fetch the geocoding data from the Mapbox Geocoding API
         fetch(geocoder)
             .then(response => response.json())
             .then(data => {
                 if (data.features.length > 0) {
+                    // If at least one location is found, extract the coordinates of the first result
                     var location = data.features[0].center;
+                    // Fly the map to the searched location with a zoom level of 14
                     map.flyTo({
                         center: location,
                         zoom: 14
                     });
 
-                    // Add a marker at the searched location
+                    // Add a marker at the searched location on the map
                     var marker = new mapboxgl.Marker().setLngLat(location).addTo(map);
                 } else {
+                    // If no location is found, display an alert to the user
                     alert('Location not found. Please try a different search term.');
                 }
             })
@@ -45,6 +51,7 @@ const router = require("express").Router();
     // Event listener for search button click
     document.getElementById('search-button').addEventListener('click', function () {
         var query = document.getElementById('search-input').value;
+        // Call the geocode function with the user's search query
         geocode(query);
     });
 
@@ -52,11 +59,11 @@ const router = require("express").Router();
     document.getElementById('search-input').addEventListener('keyup', function (event) {
         if (event.key === 'Enter') {
             var query = document.getElementById('search-input').value;
+            // Call the geocode function with the user's search query
             geocode(query);
         }
     });
+})();
 
-
-});
-
+// Export the router to make it accessible from other files
 module.exports = router;
--- controllers/user_routes.js
+++ controllers/user_routes.js
@@ -1,74 +1,84 @@
+// Import the Express router module to create routes
 const router = require("express").Router();
+
+// Import the bcrypt library for password hashing
 const { hash } = require("bcrypt");
+
+// Import the User model to interact with the database
 const User = require("../models/User");
 
-
-// Log in user
+// Route to handle user login
 router.post("/login", async (req, res) => {
     try {
+        // Check if the user is already logged in; if so, redirect to the dashboard
         if (req.session.user_id) {
             return res.redirect("/dashboard");
         }
 
+        // Extract the username and password from the request body
         const formUsername = req.body.username;
         const formPassword = req.body.password;
 
+        // Find the user in the database by their username
         const user = await User.findOne({
             where: {
                 username: formUsername
             }
         });
 
-        // If user doesn't exist, break the process with return and then redirect to /register
+        // If the user doesn't exist, redirect to the registration page
         if (!user) return res.redirect("/register");
 
-        // Validate that the password is a match
+        // Validate that the provided password matches the user's hashed password
         const isValidPass = await user.validatePass(formPassword)
 
+        // If the password is invalid, throw an error with a custom message
         if (!isValidPass) throw new Error("invalid_password");
 
-        // User has been validated and now we log the user in by creating a session
+        // If the user is valid, create a session and log the user in
         req.session.user_id = user.id;
-
-        // Give user's firstName so we can say "Welcome, [firstName]!" instead of "Welcome, [email]!"
         req.session.user_firstName = user.firstName;
 
+        // Redirect the user to the dashboard after successful login
         res.redirect("/dashboard");
 
-    } catch(err) {
+    } catch (err) {
+        // If there's an error due to an invalid password, redirect to the login page
         if (err.message === "invalid_password") {
             res.redirect("/login");
         }
     }
 });
 
-// Register user
+// Route to handle user registration
 router.post("/register", async (req, res) => {
     try {
+        // Extract user details from the request body
         const { firstName, lastName, username, email, password } = req.body;
 
-        // Check is username is already registered
+        // Check if the username is already registered
         const existingUsername = await User.findOne({
             where: {
                 username: username,
             }
         });
 
-        // Check if email is already registered
+        // Check if the email is already registered
         const existingEmail = await User.findOne({
             where: {
                 email: email,
             }
         });
 
-        // If username or email already registered, return error
+        // If the username or email is already registered, return an error
         if (existingUsername || existingEmail) {
-            return res.render("register", { error: "Username or email already registered. Please try a new username or email, or log in!"});
+            return res.render("register", { error: "Username or email already registered. Please try a new username or email, or log in!" });
         }
 
-        // Has password before saving to db
+        // Hash the password before saving it to the database
         const hashPassword = await hash(password, 10);
 
+        // Create a new user in the database with the provided details
         const newUser = await User.create({
             firstName,
             lastName,
@@ -77,22 +87,28 @@ router.post("/register", async (req, res) => {
             password: hashPassword,
         });
 
-        // Creates a session and sends a cookie to the client
+        // Create a session and send a cookie to the client after successful registration
         req.session.user_id = newUser.id;
 
+        // Redirect the user to the dashboard after successful registration
         res.redirect("/dashboard");
     } catch (err) {
+        // Check if the error is due to a duplicate email
         const dupeEmail = err.errors.find(e => e.path === "email");
-    
-        // If email already exists, redirect to the login page
+
+        // If the email already exists, redirect to the login page
         if (dupeEmail) res.redirect("/login");
     }
 });
 
+// Route to handle user logout
 router.get("/logout", (req, res) => {
+    // Destroy the session and log the user out
     req.session.destroy();
 
+    // Redirect the user to the homepage after successful logout
     res.redirect("/");
-})
+});
 
+// Export the router to make it accessible from other files
 module.exports = router;
