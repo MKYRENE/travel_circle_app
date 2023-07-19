88498965beafd478b584cbeaabb01efd78ce4736
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-19 14:29:49 -0400

    Finally got weather showing on the feed page

--- public/js/weather_el.js
+++ public/js/weather_el.js
@@ -1,18 +1,78 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("/api/weather?location=London&date=2023-07-18")
-        .then((response) => response.json())
-        .then((data) => {
-            const weatherDataDiv = document.getElementById("weatherData");
-            weatherDataDiv.innerHTML = `
-          <p>Location: ${data.location}</p>
-          <p>Date: ${data.date}</p>
-          <p>Temperature: ${data.temperature} °F</p>
-          <p>Humidity: ${data.humidity} %</p>
-          <p>Wind Speed: ${data.wind_speed} mph</p>
-          <img src="${data.weather_icon}" alt="Weather Icon">
+    const postForm = document.getElementById("postForm");
+    const location = prompt("Enter location:");
+    const date = prompt("Enter date (YYYY-MM-DD):");
+
+    function fetchWeatherData(city, date) {
+        const apiUrl = `/api/weather?location=${encodeURIComponent(city)}&date=${encodeURIComponent(date)}`;
+        return fetch(apiUrl)
+            .then((response) => response.json())
+            .catch((error) => {
+                console.error("Error fetching weather data:", error.message);
+                return null;
+            });
+    }
+
+    function displayNewPost(postData, weatherData) {
+        const mainSection = document.querySelector(".post");
+
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
+
+        // Display the content of the new post
+        postElement.innerHTML = `
+          <p>${postData.text}</p>
+          <div class="map">
+            <!-- Add any additional content for the post here (if needed) -->
+          </div>
+          <div class="weather">
+            <img src="${weatherData.weather_icon}" alt="Weather Icon">
+            <p>Location: ${weatherData.location}</p>
+            <p>Date: ${weatherData.date}</p>
+            <p>Temperature: ${weatherData.temperature} °F</p>
+            <p>Humidity: ${weatherData.humidity} %</p>
+            <p>Wind Speed: ${weatherData.wind_speed} mph</p>
+          </div>
         `;
-        })
-        .catch((error) => {
-            console.error("Error fetching weather data:", error.message);
+
+        // Add the new post to the main section
+        mainSection.appendChild(postElement);
+    }
+
+    function displayFeed(feedData) {
+        feedData.forEach((post) => {
+            displayNewPost(post, post.weatherData);
+        });
+    }
+
+    postForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        // Get the user's input from the form
+        const formData = new FormData(postForm);
+        const postContent = formData.get("text");
+        const postDate = formData.get("date");
+        const postLocation = formData.get("location");
+
+        fetchWeatherData(postLocation, postDate)
+            .then((weatherData) => {
+                if (weatherData) {
+                    const newPost = { text: postContent };
+
+                    // Display the new post in the feed along with weather information
+                    displayNewPost(newPost, weatherData);
+
+                    postForm.reset();
+                }
+            });
+    });
+
+    fetchWeatherData(location, date)
+        .then((data) => {
+            if (data) {
+                const feedData = [data];
+
+                displayFeed(feedData);
+            }
         });
 });
